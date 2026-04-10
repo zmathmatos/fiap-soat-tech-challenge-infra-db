b66be08244401c43aae59a2db04c1c4afc97bcb1
matheus matos da costa <matheus.costa@senac.br>
2026-04-10 15:11:49 -0300

    chore: rename S3 state bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "fiap-soat-terraform-state-283756756385"
+    bucket = "fiap-soat-terraform-state-bucket"
     key    = "infra-db/terraform.tfstate"
     region = "us-east-1"
   }
--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 data "terraform_remote_state" "infra_k8s" {
   backend = "s3"
   config = {
-    bucket = "fiap-soat-terraform-state-283756756385"
+    bucket = "fiap-soat-terraform-state-bucket"
     key    = "infra-k8s/terraform.tfstate"
     region = "us-east-1"
   }
